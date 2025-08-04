7f5bef55167e9661b4ec589b75b29cf34e445d1a
oilPersonage <oilfreelance@ya.ru>
2025-08-04 13:34:01 +0300

    change base vite config to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/english/",
+	base: "/",
 	plugins: [tailwindcss(), svelte()],
 	build: {
 		rollupOptions: {
