456640450d4c9a4eceb3e453c6e089ceee775b5b
oilPersonage <oilfreelance@ya.ru>
2025-08-04 13:56:20 +0300

    fix path to services-woker.js

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "./app.css";
 import App from "./App.svelte";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/service-worker.js");
+	navigator.serviceWorker.register(
+		`${import.meta.env.BASE_URL}service-worker.js`
+	);
 }
 
 const app = mount(App, {
